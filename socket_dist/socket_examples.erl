7b8fc48770ec46a40807f90c09e246b32c6cfde9
Rafal Wolak <wolak.rafal@gmail.com>
2016-07-03 00:41:19 +0200

    Fun with Sockets. Simple Server and Client

--- socket_dist/socket_examples.erl
+++ socket_dist/socket_examples.erl
@@ -1,7 +1,7 @@
 -module(socket_examples).
 
 %% API
--export([nano_get_url/0]).
+-export([nano_get_url/0, start_nano_server/0, nano_client_eval/1]).
 
 nano_get_url()->
     nano_get_url("www.google.com").
@@ -18,4 +18,41 @@ receive_data(Socket, SoFar) ->
     {tcp_closed, Socket} ->
       list_to_binary(lists:reverse(SoFar))
   end.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Server implementation        %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start_nano_server() ->
+  {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                                {reuseaddr, true},
+                                                {active, true}]),
+  {ok, Socket} = gen_tcp:accept(Listen),
+  gen_tcp:close(Listen),
+  loop(Socket).
 
+loop(Socket) ->
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Server received binary = ~p~n", [Bin]),
+      Str = binary_to_term(Bin),
+      io:format("Server (unpacked) ~p~n", [Str]),
+      Reply = lib_misc:string2value(Str),
+      io:format("Server replying = ~p~n", [Reply]),
+      gen_tcp:send(Socket, term_to_binary(Reply)),
+      loop(Socket);
+    {tcp_closed, Socket} ->
+      io:format("Server socket closed~n")
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Client implementation        %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nano_client_eval(Str) ->
+  {ok, Socket} = gen_tcp:connect("localhost", 2345, [binary, {packet, 4}]),
+  ok = gen_tcp:send(Socket, term_to_binary(Str)),
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Client received binary = ~p~n", [Bin]),
+      Val = binary_to_term(Bin),
+      io:format("Client result = ~p~n", [Val]),
+      gen_tcp:close(Socket)
+  end.
