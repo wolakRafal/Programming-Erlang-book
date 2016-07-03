f39ad73922ea463f1ea0cd1f93d73e19f35e9b26
Rafal Wolak <wolak.rafal@gmail.com>
2016-07-03 13:35:58 +0200

    Error handling on server side

--- socket_dist/socket_examples.erl
+++ socket_dist/socket_examples.erl
@@ -1,7 +1,7 @@
 -module(socket_examples).
 
 %% API
--export([nano_get_url/0, start_nano_server/0, nano_client_eval/1, start_seq_server/0, start_parallel_server/0]).
+-export([nano_get_url/0, start_nano_server/0, nano_client_eval/1, start_seq_server/0, start_parallel_server/0, error_test/0]).
 
 nano_get_url()->
     nano_get_url("www.google.com").
@@ -76,4 +76,30 @@ nano_client_eval(Str) ->
       Val = binary_to_term(Bin),
       io:format("Client result = ~p~n", [Val]),
       gen_tcp:close(Socket)
+  end.
+
+
+%% Error Handling Test
+error_test() ->
+  spawn(fun() -> error_test_server() end),
+  lib_misc:sleep(2000),
+  {ok, Socket} = gen_tcp:connect("localhost", 4321, [binary, {packet, 2}]),
+  io:format("connected to:~p~n", [Socket]),
+  gen_tcp:send(Socket, <<"123">>),
+  receive
+    Any ->
+      io:format("Any=~p~n", [Any])
+  end.
+
+error_test_server() ->
+  {ok, Listen} =  gen_tcp:listen(4321, [binary, {packet,2}]),
+  {ok, Socket} = gen_tcp:accept(Listen),
+  error_test_server_loop(Socket).
+
+error_test_server_loop(Socket) ->
+  receive
+    {tcp, Socket, Data} ->
+      io:format("Received:~p~n", [Data]),
+      _ = atom_to_list(Data),
+      error_test_server_loop(Socket)
   end.
